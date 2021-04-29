ba483cf8d46d079d2b93ac1bc97828fdfc50b372
iWarpBTC <iwarp@tuta.io>
2021-04-29 20:56:51 +0200

    random to master

--- bip39.py
+++ bip39.py
@@ -1,14 +1,45 @@
-from hashlib import sha256
+from random import getrandbits
+import hashlib 
+import hmac
 from binascii import unhexlify
 
+# nacteni wordlistu do listu
+with open("english.txt", "r", encoding="utf-8") as f:
+    wordlist = [w.strip() for w in f.readlines()]
 
-hexa = hex( int('01010011010001111111100111001101110100001001000100111011010101001011000111010100111000101100111100010100001100100000000110110111', 2 ))
+# pseudonahodne cislo
+entropy = getrandbits(128)
 
+print('pocatecni entropie (dekadicky): {}'.format(entropy))
 
-print (hexa)
+# prevedeme na hexa retezec
+hex_str_entropy = unhexlify(hex(entropy)[2:].zfill(32))
 
-stre = unhexlify(hexa[2:])
+# zahashujeme 
+check = hashlib.sha256(hex_str_entropy).hexdigest()
+print('checksum hash entropie: {}'.format(check))
 
-hasher = sha256()
-hasher.update(stre)
-print( hasher.hexdigest() )
+print(bin(entropy)[2:])
+print(bin(int(check, 16))[2:].zfill(256)[:4])
+
+# pripojime 4 bity na konec entropie
+b = bin(entropy)[2:].zfill(128) + bin(int(check, 16))[2:].zfill(256)[:4]
+
+print(b)
+
+# prevedeme na slova
+result = []
+for i in range(len(b) // 11):
+    idx = int(b[i * 11 : (i + 1) * 11], 2)
+    result.append(wordlist[idx])
+    result_phrase = " ".join(result)
+    
+print (result_phrase)
+
+passphrase = input('zadej passphrase: ')
+
+seed = hashlib.pbkdf2_hmac("sha512", result_phrase.encode("utf-8"), ('mnemonic'+passphrase).encode("utf-8"), 2048)
+print('seed: {}'.format(seed.hex()))
+
+master = hmac.new(b"Bitcoin seed", seed, digestmod=hashlib.sha512).digest()
+print('master priv: {}'.format(master.hex()))
